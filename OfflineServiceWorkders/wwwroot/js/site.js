2c2db0311902baf63b948f294b98462a72cdbd8d
derrickhampton <sacstate2600@gmail.com>
2021-01-26 11:13:54 -0800

    * .gitignore: Offline Service Workers
    * OfflineServiceWorkders.sln:
    * Startup.cs:
    * Program.cs:
    * sw.js:
    * appsettings.json:
    * Index.cshtml:
    * Error.cshtml:
    * site.js:
    * favicon.ico:
    * Privacy.cshtml:
    * site.css:
    * Index.cshtml.cs:
    * Error.cshtml.cs:
    * Privacy.cshtml.cs:
    * _ViewStart.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * OfflineServiceWorkders.csproj:
    * launchSettings.json:
    * LICENSE.txt:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * jquery.validate.js:
    * LICENSE.txt:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.css.map:
    * bootstrap.bundle.min.js:
    * bootstrap-grid.min.css:
    * bootstrap-reboot.css.map:
    * bootstrap-reboot.min.css:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap.bundle.min.js.map:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ OfflineServiceWorkders/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿//TODO:Add local storage for form here!
+//TODO:Add hooks for going offline and coming back online!
+
+
+
+/*var assests = '';
+function assetRender() {
+    var assets = '';
+    var assetFolders = ['js', 'css'];
+    for (var x = 0; x < assetFolders.length; x++) {
+        assests += _getAllFilesFromFolder(assetFolders[x]);
+    }
+
+    return assests;
+}
+
+var _getAllFilesFromFolder = function (dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function (file) {
+
+        file = dir + '/' + file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
+console.log('Got Files: '+assetRender());*/
+
+
+navigator.serviceWorker.register('sw.js');
---
+++ OfflineServiceWorkders/wwwroot/sw.js
@@ -0,0 +1,122 @@
+﻿"use strict";
+console.log('Loading');
+/*function assetRender() {
+    var assets = '';
+    var assetFolders = ['js', 'css', 'Pages'];
+    for (var x = 0; x < assetFolders.length; x++) {
+        assests += _getAllFilesFromFolder(assetFolders[x]);
+    }
+
+    return assests;
+}
+
+var _getAllFilesFromFolder = function (dir) {
+
+    var filesystem = require("fs");
+    var results = [];
+
+    filesystem.readdirSync(dir).forEach(function (file) {
+
+        file = dir + '/' + file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};*/
+//;http://localhost:5000
+var serviceWorkerVersion = 'v3::';
+//TODO: Add fucntion to populate assest by traversing selected folders
+//TODO: Show an example with minumal assests
+
+var offlineAssets = ['',
+    'css/site.css',
+    'favicon.ico',
+    'js/site.js',
+    'lib/jquery/dist/jquery.min.js',
+    'lib/bootstrap/dist/js/bootstrap.bundle.min.js',
+    'lib/bootstrap/dist/css/bootstrap.min.css']; //assetRender()
+
+//];
+
+console.log('Service Worker: Running!');
+
+//Service Worker install event
+
+self.addEventListener('install', function (event) {
+    self.skipWaiting();//Function to remove the older service worker version
+
+    console.log('Service Worker: Install in Progress!');
+    event.waitUntil(
+        caches.open(serviceWorkerVersion + 'offlineAssets')
+            .then(function(cache){
+            return cache.addAll(offlineAssets);
+        })
+    );
+});
+
+//Service Worker fetch event
+
+self.addEventListener("fetch", function (event) {
+    console.log('Service Worker: Fetch in Progress!');
+
+    //Only fetch on get requests and ingnore all others  
+  //  if (event.request.method !== 'GET') {
+  //      return;
+   // }
+   
+    event.respondWith(caches.match(event.request).then(function (cached) {
+                
+                var networkEvent = fetch(event.request).then(fetchedFromNetwork, unableToResolve).catch(unableToResolve);
+                return cached || networkEvent;
+
+                function fetchedFromNetwork(response) {
+                    var cacheCopy = response.clone();
+                    caches.open(version + 'pages').then(function add(cache) {
+
+                            return cache.put(event.request, cacheCopy);
+                        }).then(function () {
+                            console.log('Service Worker: Fetch Stored in Cache!', event.request.url);
+                        });
+
+                    return response;
+                }
+ 
+                function unableToResolve() {
+                    
+                    console.log('Service Worker: Fetch Failed! Network Issue or No Updates');
+                    return new Response('<h1>Service Unavailable!</h1>', {
+                        status: 503,
+                        statusText: 'Service Unavailable',
+                        headers: new Headers({
+                            'Content-Type': 'text/html'
+                        })
+                    });
+                }
+            })
+    );
+});
+
+//Service Worker activate event
+self.addEventListener("activate", function (event) {
+    console.log('Service Worker: Activate in Progress!');
+
+    event.waitUntil(
+        caches.keys().then(function (keys) {
+                return Promise.all(keys.filter(function (key) {
+                    return !key.startsWith(serviceWorkerVersion);
+                        }).map(function (key) {
+                            return caches.delete(key);
+                        })
+                );
+            })
+            .then(function () {
+                console.log('Service Worker: Activate Finished!');
+            })
+    );
+});
