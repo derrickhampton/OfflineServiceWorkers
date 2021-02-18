a8a1453321b1d2195ae4019959e3fced458fa18d
derrickhampton <sacstate2600@gmail.com>
2021-02-17 17:38:12 -0800

    Removed comments and fixed cache name in fetch event

--- OfflineServiceWorkders/wwwroot/sw.js
+++ OfflineServiceWorkders/wwwroot/sw.js
@@ -1,35 +1,6 @@
 ﻿"use strict";
 console.log('Loading');
-/*function assetRender() {
-    var assets = '';
-    var assetFolders = ['js', 'css', 'Pages'];
-    for (var x = 0; x < assetFolders.length; x++) {
-        assests += _getAllFilesFromFolder(assetFolders[x]);
-    }
 
-    return assests;
-}
-
-var _getAllFilesFromFolder = function (dir) {
-
-    var filesystem = require("fs");
-    var results = [];
-
-    filesystem.readdirSync(dir).forEach(function (file) {
-
-        file = dir + '/' + file;
-        var stat = filesystem.statSync(file);
-
-        if (stat && stat.isDirectory()) {
-            results = results.concat(_getAllFilesFromFolder(file))
-        } else results.push(file);
-
-    });
-
-    return results;
-
-};*/
-//;http://localhost:5000
 var serviceWorkerVersion = 'v5::';
 //TODO: Add fucntion to populate assest by traversing selected folders
 //TODO: Show an example with minumal assests
@@ -40,14 +11,11 @@ var offlineAssets = ['',
     'js/site.js',
     'lib/jquery/dist/jquery.min.js',
     'lib/bootstrap/dist/js/bootstrap.bundle.min.js',
-    'lib/bootstrap/dist/css/bootstrap.min.css']; //assetRender()
-
-//];
+    'lib/bootstrap/dist/css/bootstrap.min.css']; 
 
 console.log('Service Worker: Running!');
 
-//Service Worker install event
-
+//Service Worker install event 
 self.addEventListener('install', function (event) {
     self.skipWaiting();//Function to remove the older service worker version
 
@@ -60,24 +28,16 @@ self.addEventListener('install', function (event) {
     );
 });
 
-//Service Worker fetch event
-
+//Service Worker fetch event  
 self.addEventListener("fetch", function (event) {
     console.log('Service Worker: Fetch in Progress!');
-
-    //Only fetch on get requests and ingnore all others  
-  //  if (event.request.method !== 'GET') {
-  //      return;
-   // }
-   
-    event.respondWith(caches.match(event.request).then(function (cached) {
                 
                 var networkEvent = fetch(event.request).then(fetchedFromNetwork, unableToResolve).catch(unableToResolve);
                 return cached || networkEvent;
 
                 function fetchedFromNetwork(response) {
                     var cacheCopy = response.clone();
-                    caches.open(version + 'pages').then(function add(cache) {
+                    caches.open(version + 'offlineAssets').then(function add(cache) {
 
                             return cache.put(event.request, cacheCopy);
                         }).then(function () {
